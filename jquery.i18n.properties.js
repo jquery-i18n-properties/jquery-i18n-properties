00ba32bbcf35de18c58f2421db11feeabfc0da9e
Matthew Lohbihler <ml@serotoninsoftware.com>
2011-05-25 14:19:34 +0000

    See http://code.google.com/p/jquery-i18n-properties/issues/detail?id=5 for extended commit comments.
    git-svn-id: http://jquery-i18n-properties.googlecode.com/svn/trunk@4 60ed876c-c37a-d86c-05f3-22939075770b

--- jquery.i18n.properties.js
+++ jquery.i18n.properties.js
@@ -37,10 +37,12 @@ $.i18n.map = {};
  *      language:  'en_US',
  *      path:      'bundles'
  * });
- * @param  name			(string/string[], required) names of file to load (eg, 'Messages' or ['Msg1','Msg2'])
+ * @param  name			(string/string[], optional) names of file to load (eg, 'Messages' or ['Msg1','Msg2']). Defaults to "Messages"
  * @param  language		(string, optional) language/country code (eg, 'en', 'en_US', 'pt_PT'). if not specified, language reported by the browser will be used instead.
  * @param  path			(string, optional) path of directory that contains file to load
  * @param  mode			(string, optional) whether bundles keys are available as JavaScript variables/functions or as a map (eg, 'vars' or 'map')
+ * @param  cache        (boolean, optional) whether bundles should be cached by the browser, or forcibly reloaded on each page load. Defaults to false (i.e. forcibly reloaded)
+ * @param  encoding 	(string, optional) the encoding to request for bundles. Property file resource bundles are specified to be in ISO-8859-1 format. Defaults to UTF-8 for backward compatibility.
  * @param  callback     (function, optional) callback function to be called after script is terminated
  */
 $.i18n.properties = function(settings) {
@@ -50,7 +52,9 @@ $.i18n.properties = function(settings) {
         language:       '',
         path:           '',  
         mode:           'vars',
-        callback:       function(){}
+        cache:			false,
+        encoding:       'UTF-8',
+        callback:       null
     };
     settings = $.extend(defaults, settings);    
     if(settings.language === null || settings.language == '') {
@@ -62,14 +66,14 @@ $.i18n.properties = function(settings) {
 	var files = getFiles(settings.name);
 	for(i=0; i<files.length; i++) {
 		// 1. load base (eg, Messages.properties)
-		loadAndParseFile(settings.path + files[i] + '.properties', settings.language, settings.mode);
+		loadAndParseFile(settings.path + files[i] + '.properties', settings);
         // 2. with language code (eg, Messages_pt.properties)
 		if(settings.language.length >= 2) {
-            loadAndParseFile(settings.path + files[i] + '_' + settings.language.substring(0, 2) +'.properties', settings.language, settings.mode);
+            loadAndParseFile(settings.path + files[i] + '_' + settings.language.substring(0, 2) +'.properties', settings);
 		}
 		// 3. with language code and country code (eg, Messages_pt_PT.properties)
         if(settings.language.length >= 5) {
-            loadAndParseFile(settings.path + files[i] + '_' + settings.language.substring(0, 5) +'.properties', settings.language, settings.mode);
+            loadAndParseFile(settings.path + files[i] + '_' + settings.language.substring(0, 5) +'.properties', settings);
         }
 	}
 	
@@ -82,19 +86,150 @@ $.i18n.properties = function(settings) {
  * When configured with mode: 'map', allows access to bundle values by specifying its key.
  * Eg, jQuery.i18n.prop('com.company.bundles.menu_add')
  */
-$.i18n.prop = function(key, placeHolderValues) {
+$.i18n.prop = function(key /* Add parameters as function arguments as necessary  */) {
 	var value = $.i18n.map[key];
-	if(value == null) { return '[' + key + ']'; }
-	if(!placeHolderValues) {
-    //if(key == 'spv.lbl.modified') {alert(value);}
-		return value;
-	}else{
-		for(var i=0; i<placeHolderValues.length; i++) {
-			var regexp = new RegExp('\\{('+i+')\\}', "g");
-			value = value.replace(regexp, placeHolderValues[i]);
+	if (value == null)
+		return '[' + key + ']';
+	
+//	if(arguments.length < 2) // No arguments.
+//    //if(key == 'spv.lbl.modified') {alert(value);}
+//		return value;
+	
+//	if (!$.isArray(placeHolderValues)) {
+//		// If placeHolderValues is not an array, make it into one.
+//		placeHolderValues = [placeHolderValues];
+//		for (var i=2; i<arguments.length; i++)
+//			placeHolderValues.push(arguments[i]);
+//	}
+
+	// Place holder replacement
+	/**
+	 * Tested with:
+	 *   test.t1=asdf ''{0}''
+	 *   test.t2=asdf '{0}' '{1}'{1}'zxcv
+	 *   test.t3=This is \"a quote" 'a''{0}''s'd{fgh{ij'
+	 *   test.t4="'''{'0}''" {0}{a}
+	 *   test.t5="'''{0}'''" {1}
+	 *   test.t6=a {1} b {0} c
+	 *   test.t7=a 'quoted \\ s\ttringy' \t\t x
+	 *
+	 * Produces:
+	 *   test.t1, p1 ==> asdf 'p1'
+	 *   test.t2, p1 ==> asdf {0} {1}{1}zxcv
+	 *   test.t3, p1 ==> This is "a quote" a'{0}'sd{fgh{ij
+	 *   test.t4, p1 ==> "'{0}'" p1{a}
+	 *   test.t5, p1 ==> "'{0}'" {1}
+	 *   test.t6, p1 ==> a {1} b p1 c
+	 *   test.t6, p1, p2 ==> a p2 b p1 c
+	 *   test.t6, p1, p2, p3 ==> a p2 b p1 c
+	 *   test.t7 ==> a quoted \ s	tringy 		 x
+	 */
+	
+	var i;
+	if (typeof(value) == 'string') {
+        // Handle escape characters. Done separately from the tokenizing loop below because escape characters are 
+		// active in quoted strings.
+        i = 0;
+        while ((i = value.indexOf('\\', i)) != -1) {
+ 		   if (value[i+1] == 't')
+ 			   value = value.substring(0, i) + '\t' + value.substring((i++) + 2); // tab
+ 		   else if (value[i+1] == 'r')
+ 			   value = value.substring(0, i) + '\r' + value.substring((i++) + 2); // return
+ 		   else if (value[i+1] == 'n')
+ 			   value = value.substring(0, i) + '\n' + value.substring((i++) + 2); // line feed
+ 		   else if (value[i+1] == 'f')
+ 			   value = value.substring(0, i) + '\f' + value.substring((i++) + 2); // form feed
+ 		   else if (value[i+1] == '\\')
+ 			   value = value.substring(0, i) + '\\' + value.substring((i++) + 2); // \
+ 		   else
+ 			   value = value.substring(0, i) + value.substring(i+1); // Quietly drop the character
+        }
+		
+		// Lazily convert the string to a list of tokens.
+		var arr = [], j, index;
+		i = 0;
+		while (i < value.length) {
+			if (value[i] == '\'') {
+				// Handle quotes
+				if (i == value.length-1)
+					value = value.substring(0, i); // Silently drop the trailing quote
+				else if (value[i+1] == '\'')
+					value = value.substring(0, i) + value.substring(++i); // Escaped quote
+				else {
+					// Quoted string
+					j = i + 2;
+					while ((j = value.indexOf('\'', j)) != -1) {
+						if (j == value.length-1 || value[j+1] != '\'') {
+							// Found start and end quotes. Remove them
+							value = value.substring(0,i) + value.substring(i+1, j) + value.substring(j+1);
+							i = j - 1;
+							break;
+						}
+						else {
+							// Found a double quote, reduce to a single quote.
+							value = value.substring(0,j) + value.substring(++j);
+						}
+					}
+					
+					if (j == -1) {
+						// There is no end quote. Drop the start quote
+						value = value.substring(0,i) + value.substring(i+1);
+					}
+				}
+			}
+			else if (value[i] == '{') {
+				// Beginning of an unquoted place holder.
+				j = value.indexOf('}', i+1);
+				if (j == -1)
+					i++; // No end. Process the rest of the line. Java would throw an exception
+				else {
+					// Add 1 to the index so that it aligns with the function arguments.
+					index = parseInt(value.substring(i+1, j));
+					if (!isNaN(index) && index >= 0) {
+						// Put the line thus far (if it isn't empty) into the array
+						var s = value.substring(0, i);
+						if (s != "")
+							arr.push(s);
+						// Put the parameter reference into the array
+						arr.push(index);
+						// Start the processing over again starting from the rest of the line.
+						i = 0;
+						value = value.substring(j+1);
+					}
+					else
+						i = j + 1; // Invalid parameter. Leave as is.
+				}
+			}
+			else
+				i++;
 		}
-		return value;
+		
+		// Put the remainder of the no-empty line into the array.
+		if (value != "")
+			arr.push(value);
+		value = arr;
+		
+		// Make the array the value for the entry.
+		$.i18n.map[key] = arr;
 	}
+	
+	if (value.length == 0)
+		return "";
+	if (value.lengh == 1 && typeof(value[0]) == "string")
+		return value[0];
+	
+	var s = "";
+	for (i=0; i<value.length; i++) {
+		if (typeof(value[i]) == "string")
+			s += value[i];
+		// Must be a number
+		else if (value[i] + 1 < arguments.length)
+			s += arguments[value[i] + 1];
+		else
+			s += "{"+ value[i] +"}";
+	}
+	
+	return s;
 };
 
 /** Language reported by browser, normalized code */
@@ -104,15 +239,15 @@ $.i18n.browserLang = function() {
 
 
 /** Load and parse .properties files */
-function loadAndParseFile(filename, language, mode) {
+function loadAndParseFile(filename, settings) {
 	$.ajax({
         url:        filename,
         async:      false,
-        cache:		false,
-        contentType:'text/plain;charset=UTF-8',
+        cache:		settings.cache,
+        contentType:'text/plain;charset='+ settings.encoding,
         dataType:   'text',
         success:    function(data, status) {
-        				parseData(data, mode); 
+        				parseData(data, settings.mode); 
 					}
     });
 }
@@ -137,9 +272,9 @@ function parseData(data, mode) {
                		value = value.substring(0, value.length - 1);
                		value += parameters[++i].replace( /\s\s*$/, '' ); // right trim
                }               
+               // Put values with embedded '='s back together
                for(var s=2;s<pair.length;s++){ value +='=' + pair[s]; }
-               value = value.replace( /"/g, '\\"' ); // escape quotation mark (")
-               value = value.replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim  
+               value = value.replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim
                
                /** Mode: bundle keys in a map */
                if(mode == 'map' || mode == 'both') {
@@ -156,6 +291,8 @@ function parseData(data, mode) {
                
                /** Mode: bundle keys as vars/functions */
                if(mode == 'vars' || mode == 'both') {
+                   value = value.replace( /"/g, '\\"' ); // escape quotation mark (")
+                   
                    // make sure namespaced key exists (eg, 'some.key') 
                    checkKeyNamespace(name);
                    
@@ -191,6 +328,7 @@ function parseData(data, mode) {
 }
 
 /** Make sure namespace exists (for keys with dots in name) */
+// TODO key parts that start with numbers quietly fail. i.e. month.short.1=Jan
 function checkKeyNamespace(key) {
 	var regDot = /\./;
 	if(regDot.test(key)) {
